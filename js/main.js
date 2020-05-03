03fe60396575b44e6cb355d6a0dfe666dec6faf7
Noor Al Darraji <49676151+nooraldarraji@users.noreply.github.com>
2020-05-03 03:22:03 -0500

    2020 Update, still need more info tho :hammer:

--- js/main.js
+++ js/main.js
@@ -1,21 +1,31 @@
 var doc = jQuery(document)
 
-doc.ready(function() {
+
+doc.ready(function () {
 
     'use strict';
 
     $(window).scrollTop(0) //  Re-set the scroll value to zero so the navbar color won't get faded on refresh.
 
-    var typed = new Typed('.noorname-desc', {
-        strings: ['Hello, my name is Noor, I am Full-Stack Web Developer, feel free to take a look at my latest projects and contact me using any of my socials below.'],
-        smartBackspace: true, // Default value
-        loop: false,
-        backDelay: 1000,
-        showCursor: true,
-        cursorChar: '_',
-        autoInsertCss: true,
-        typeSpeed: 40
+
+    $(window).on('load', function () {
+
+        $('.site-loader').fadeOut(3000)
+        setTimeout(function () {
+            var typed = new Typed('.noorname-desc', {
+                strings: ['Hello, my name is Noor, I am Full-Stack Web Developer, feel free to take a look at my latest projects and contact me using any of my socials below.'],
+                smartBackspace: true, // Default value
+                loop: false,
+                backDelay: 1000,
+                showCursor: true,
+                cursorChar: '_',
+                autoInsertCss: true,
+                typeSpeed: 40
+            })
+
+        }, 3000)
     })
+
 
     $('.navbar-nav').onePageNav({
         currentClass: 'active'
@@ -26,7 +36,7 @@ if ($(window).scrollTop() < 200) {
     $('.navbar-nav').addClass('navbar-nav-bg')
 }
 
-$(window).scroll(function() {
+$(window).scroll(function () {
 
     var top = $(window).scrollTop()
 
@@ -45,11 +55,11 @@ $(window).scroll(function() {
 
     }
 
-    $('.progress-content .skill-progress').each(function() {
+    $('.progress-content .skill-progress').each(function () {
 
         var waypoint = new Waypoint({
             element: this,
-            handler: function(direction) {
+            handler: function (direction) {
                 var value = $(this.element).attr('data-progress')
                 $(this.element).css('width', '' + value + '%')
             },
@@ -61,7 +71,7 @@ $(window).scroll(function() {
         itemSelector: '.projects-item'
     })
 
-    $('.filter li').on('click', function() {
+    $('.filter li').on('click', function () {
         $('.filter li').removeClass('filter-active')
         var filterValue = $(this).attr('data-filter')
         $grid.isotope({ filter: filterValue })
